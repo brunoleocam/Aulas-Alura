1ffd19cbfe37911a7cc088bf268991918a5054e3
brunoleocam <brunoleocam@gmail.com>
2022-02-24 09:15:36 -0300

    banco.php

--- PHP/banco.php
+++ PHP/banco.php
@@ -12,14 +12,23 @@ $contasCorrentes = [
         'saldo' => 100]
 ];
 
-$contasCorrentes['123.456.789-10']['saldo'] -= 500;
-
-if( 500 >$contasCorrentes['123.456.489-11']['saldo']){
-    echo "Você não pode sacar este valor".PHP_EOL;
-} else {
-    $contasCorrentes['123.456.489-11']['saldo'] -= 500;
+function sacar($conta, $valorASacar){
+    if( $valorASacar > $conta['saldo']){
+        exibeMensagem("Você não pode sacar este valor");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+    return $conta;
 }
 
+function exibeMensagem($mensagem) {
+    echo $mensagem.PHP_EOL;
+}
+
+$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'],500);
+$contasCorrentes['123.256.789-10'] = sacar($contasCorrentes['123.256.789-10'],500);
+
+
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf." ".$conta['titular']." ".$conta['saldo'].PHP_EOL;
+    exibeMensagem($cpf." ".$conta['titular']." ".$conta['saldo']);
 }
